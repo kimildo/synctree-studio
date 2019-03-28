ee752b28be756982024448013f28d40a81eda734
kimildo <kimildo@saramin.co.kr>
2019-03-28 11:34:10 +0900

    Update GeneratebPk6Bidk0jrzhNQkKprzhsController.php

--- GeneratebPk6Bidk0jrzhNQkKprzhsController.php
+++ GeneratebPk6Bidk0jrzhNQkKprzhsController.php
@@ -64,7 +64,7 @@ class GeneratebPk6Bidk0jrzhNQkKprzhsController extends Synctree
 
 		    $bizOps = json_decode(static::BIZOPS, true);
 
-			$responseDatas = [];
+		    $responseDatas = [];
 		    $result['request'] = [
 		        'actor_alias' => $bizOps['actor_alias'] ?? null,
 		        'request' => $this->params,
